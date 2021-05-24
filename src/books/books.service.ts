855f6fedeaff6b7acf6cf00e1d1a6f3cf382984d
Vicky <vrod8763@gmail.com>
2021-05-24 15:10:27 -0400

    add methods to delete and add book

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, HttpException } from '@nestjs/common';
+import { BOOKS } from '../mocks/books.mock';
 @Injectable()
-export class BooksService {}
+export class BooksService {
+  books = BOOKS;
+  addBook(book): Promise<any> {
+    return new Promise((resolve) => {
+      this.books.push(book);
+      resolve(this.books);
+    });
+  }
+
+  deleteBook(bookId): Promise<any> {
+    const id = Number(bookId);
+    return new Promise((resolve) => {
+      const index = this.books.findIndex((book) => book.id === id);
+      if (index === -1) {
+        throw new HttpException('Book does not exist!', 404);
+      }
+      this.books.splice(1, index);
+      resolve(this.books);
+    });
+  }
+}
