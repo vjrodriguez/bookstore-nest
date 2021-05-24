e8fbf0c8d437c27b105bfda41ddee1f7bfde1baf
Vicky <vrod8763@gmail.com>
2021-05-24 15:24:05 -0400

    inject the service into the controller

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
-
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Query,
+  Delete,
+} from '@nestjs/common';
+import { BookService } from './books.service';
+import { CreateBookDTO } from './dto/create-book.CreateBookDTO';
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  constructor(private booksService: BookService) {}
+
+  @Get()
+  async getBooks() {
+    const books = await this.booksService.getBooks();
+    return books;
+  }
+
+  @Get(':bookID')
+  async getBook(@Param('bookID') bookID) {
+    const book = await this.booksService.getBook(bookID);
+    return book;
+  }
+
+  @Post()
+  async addBook(@Body() CreateBookDTO: CreateBookDTO) {
+    const book = await this.booksService.addBook(CreateBookDTO);
+    return book;
+  }
+
+  @Delete()
+  async deleteBook(@Query() query) {
+    const books = await this.booksService.deleteBook(query.bookID);
+    return books;
+  }
+}
